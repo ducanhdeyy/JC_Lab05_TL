4a068f7a9a6cb77e70a6a3f92a18751b58cfc272
Anh <lanhnhubang2k2@gmail.com>
2022-12-18 20:09:14 +0700

    update

---
+++ bai4/main.java
@@ -0,0 +1,14 @@
+package bai4;
+
+public class main {
+
+	public static void main(String[] args) {
+		 Cylinder cylinder = new Cylinder(7.1f,"red",5);
+	        System.out.println("the tich hinh tron la:");
+	        System.out.println(cylinder.getVolume());
+	        System.out.println("\n thong tin hinh tron la:");
+	        System.out.println(cylinder);
+
+	}
+
+}
